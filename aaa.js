4790f3fc3af05b9fad01a92fad024f46f59babc4
kimdohyung <game5780@wjtb.net>
2021-11-19 11:40:36 +0900

    first commit

---
+++ aaa.js
@@ -0,0 +1,5 @@
+
+
+loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf 
+loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf 
+loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf loasdfa sdfasdf asdf asdf 
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1a2c2c8d-a6c5-4e83-8463-03e0e7675599'
+};
